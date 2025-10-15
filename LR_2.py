25d2d06b2db2bb461f006e2561443e8b452ebbb5
Dmitry <thedeath478@gmail.com>
2025-10-15 14:25:04 +0300

    Delete LR_2.py

--- LR_2.py
+++
@@ -1,9 +0,0 @@
-def two_sum(nums, b):
-    a = {} 
-
-    for i, num in enumerate(nums):
-        complement = b - num 
-        if complement in a:
-            return [a[complement], i] 
-        a[num] = i 
-    return None
