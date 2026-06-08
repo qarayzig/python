8d3ac28f6d3ecb947cdd93096d81986614ab5be6
Dmitry <thedeath478@gmail.com>
2026-06-09 00:59:28 +0300

    Add files via upload

---
+++ Лабораторная 2/unittest.py
@@ -0,0 +1,18 @@
+import unittest
+from sum import sum
+
+
+class TestSum(unittest.TestCase):
+
+    def test_example_1(self):
+        self.assertEqual(sum([2, 7, 11, 15], 9), [0, 1])
+
+    def test_example_2(self):
+        self.assertEqual(sum([3, 2, 4], 6), [1, 2])
+
+    def test_example_3(self):
+        self.assertEqual(sum([3, 3], 6), [0, 1])
+
+
+if __name__ == "__main__":
+    unittest.main(argv=[''], verbosity=2, exit=False)
